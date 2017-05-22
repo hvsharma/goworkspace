4f7a614aab63b5588f7ff619c91a2cf931ae5e9c
hvsharma <hvsharma_learning@outlook.com>
2017-05-22 18:17:41 +0530

    Added JSON parsing demo + Added proxy configuration for accessing from Office PC

--- src/25-webrequest-demo/webrequest-demo.go
+++ src/25-webrequest-demo/webrequest-demo.go
@@ -4,12 +4,19 @@ import (
 	"fmt"
 	"io/ioutil"
 	"net/http"
+	"net/url"
+	"os"
+	"time"
 )
 
 func main() {
-	url := "https://api.github.com/users/hvsharma"
-	fmt.Println("Trying to retrieve data from:", url)
-	response, err := http.Get(url)
+
+	serviceURL := "https://api.github.com/users/hvsharma"
+	fmt.Println("Trying to retrieve data from:", serviceURL)
+
+	client := getHTTPClient()
+
+	response, err := client.Get(serviceURL)
 	if err != nil {
 		fmt.Println(err)
 	} else {
@@ -23,4 +30,34 @@ func main() {
 			fmt.Println("Response from url:", result)
 		}
 	}
+}
+
+/*
+	getHTTPClient : Added to include the proxy from my own office
+	It's best practice to include our own client instead of directly using http.get because
+	of the timeout. Source : https://medium.com/@nate510/don-t-use-go-s-default-http-client-4804cb19f779
+*/
+func getHTTPClient() *http.Client {
+	var client *http.Client
+	hostname, _ := os.Hostname()
+
+	if hostname == "01HW477850" {
+		proxyString := "http://www.mc.xerox.com:8000"
+		proxyURL, _ := url.Parse(proxyString)
+		tr := &http.Transport{
+			Proxy:              http.ProxyURL(proxyURL),
+			MaxIdleConns:       10,
+			IdleConnTimeout:    30 * time.Second,
+			DisableCompression: true,
+		}
+
+		client = &http.Client{
+			Transport: tr,
+			Timeout:   time.Second * 10}
+
+	} else {
+		client = &http.Client{Timeout: time.Second * 10}
+	}
+
+	return client
 }
--- src/26-parsing-json-demo/parsing-json-demo.go
+++ src/26-parsing-json-demo/parsing-json-demo.go
@@ -1,21 +1,54 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"net/http"
+	"net/url"
+	"os"
+	"strings"
+	"time"
 )
 
-func main() {
-	url := "https://api.github.com/search/users?q=hvs"
-	content := contentFromServer(url)
-	getUsersFromJSON(content)
+/*
+	Made Two struct types to parse the response from the github API
+*/
+
+//User : Stores user data
+type User struct {
+	Login string
+	Id    int
+	Url   string
 }
 
-func contentFromServer(url string) string {
+//UsersWrapper : Stores user data
+type UsersWrapper struct {
+	Total_count        int
+	Incomplete_results bool
+	Items              []User
+}
+
+func main() {
+	serviceURL := "https://api.github.com/search/users?q=hvs"
+	client := getHTTPClient()
+	content := getContentFromServer(serviceURL, client)
+	users := getUsersFromJSON(content)
+	if users != nil {
+		for _, user := range users {
+			fmt.Printf("\nUsername:\t%v\nUser ID:\t%v\nURL:\t\t%v\n", user.Login, user.Id, user.Url)
+			fmt.Println()
+		}
+	} else {
+		fmt.Println("No data available for this query...!")
+	}
+
+}
+
+func getContentFromServer(serviceURL string, client *http.Client) string {
 	var result string
-	fmt.Println("Trying to retrieve data from:", url)
-	response, err := http.Get(url)
+	fmt.Println("Trying to retrieve data from:", serviceURL)
+	response, err := client.Get(serviceURL)
 	if err != nil {
 		fmt.Println(err)
 	} else {
@@ -26,12 +59,64 @@ func contentFromServer(url string) string {
 			fmt.Println(err)
 		} else {
 			result = string(bytes)
-			fmt.Println("Response from url:", result)
+			if len(strings.TrimSpace(result)) > 0 {
+				fmt.Println("Response from url retrieved. Trying to parse Response...")
+			} else {
+				fmt.Println("Empty Response...")
+			}
 		}
 	}
 	return result
 }
 
-func getUsersFromJSON(data string) {
+func getUsersFromJSON(data string) []User {
+	var usersWrapper UsersWrapper
+	decoder := json.NewDecoder(strings.NewReader(data))
 
+	for decoder.More() {
+		err := decoder.Decode(&usersWrapper)
+		checkError(err)
+		fmt.Println("Total Items Present:", usersWrapper.Total_count)
+		if usersWrapper.Total_count > 0 {
+			return usersWrapper.Items
+		}
+	}
+
+	return nil
+}
+
+func checkError(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
+
+/*
+	getHTTPClient : Added to include the proxy from my own office
+	It's best practice to include our own client instead of directly using http.get because
+	of the timeout. Source : https://medium.com/@nate510/don-t-use-go-s-default-http-client-4804cb19f779
+*/
+func getHTTPClient() *http.Client {
+	var client *http.Client
+	hostname, _ := os.Hostname()
+
+	if hostname == "01HW477850" {
+		proxyString := "http://www.mc.xerox.com:8000"
+		proxyURL, _ := url.Parse(proxyString)
+		tr := &http.Transport{
+			Proxy:              http.ProxyURL(proxyURL),
+			MaxIdleConns:       10,
+			IdleConnTimeout:    30 * time.Second,
+			DisableCompression: true,
+		}
+
+		client = &http.Client{
+			Transport: tr,
+			Timeout:   time.Second * 10}
+
+	} else {
+		client = &http.Client{Timeout: time.Second * 10}
+	}
+
+	return client
 }
